95ea80f143af560154017038857f4f8d8413f23f
Dustinaevans <Dustinaevans@gmail.com>
2012-10-08 14:41:12 +0000

    Adding main (String[] args) class

---
+++ Start.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package openPOS;
+
+/**
+ * @author Dustin Evans
+ *
+ */
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		new StartScreen();
+
+	}
+
+}
